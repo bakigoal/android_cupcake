85dc3d05c8bf54a40b12a667e5e848d2acf94c24
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-05 19:02:51 +0500

    calculate price

--- app/src/main/java/com/example/cupcake/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/viewmodel/OrderViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.ViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
 class OrderViewModel : ViewModel() {
 
     private var _quantity = MutableLiveData<Int>()
@@ -28,6 +31,7 @@ class OrderViewModel : ViewModel() {
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
+        updatePrice()
     }
 
     fun setFlavor(flavor: String) {
@@ -36,10 +40,16 @@ class OrderViewModel : ViewModel() {
 
     fun setDate(date: String) {
         this._date.value = date
+        updatePrice()
     }
 
-    fun setPrice(price: Double) {
-        _price.value = price
+    private fun updatePrice() {
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        // If the user selected the first option (today) for pickup, add the surcharge
+        if (dateOptions[0] == _date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
     }
 
     fun hasNoFlavorSet(): Boolean {
