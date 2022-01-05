4244ef621bca0866090d40490878adc64da0dfaa
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-05 20:08:45 +0500

    send text intent

--- app/src/main/java/com/example/cupcake/fragment/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/fragment/SummaryFragment.kt
@@ -1,13 +1,14 @@
 package com.example.cupcake.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.cupcake.R
 import com.example.cupcake.databinding.FragmentSummaryBinding
 import com.example.cupcake.viewmodel.OrderViewModel
 
@@ -33,7 +34,23 @@ class SummaryFragment : Fragment() {
     }
 
     fun sendOrder() {
-        Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
+        val numberOfCupcakes = sharedViewModel.quantity.value ?: 0
+        val orderSummary = getString(
+            R.string.order_details,
+            resources.getQuantityString(R.plurals.cupcakes, numberOfCupcakes, numberOfCupcakes),
+            sharedViewModel.flavor.value.toString(),
+            sharedViewModel.date.value.toString(),
+            sharedViewModel.price.value.toString()
+        )
+
+        val intent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_cupcake_order))
+            .putExtra(Intent.EXTRA_TEXT, orderSummary)
+
+        if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
+            startActivity(intent)
+        }
     }
 
     fun cancelOrder() {
